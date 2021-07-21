25f5572152a4666d53f77117a41e5edc82e0fe6e
amilz <85324096+amilz@users.noreply.github.com>
2021-07-20 19:25:02 -0700

    solletandphantom work

--- src/index.js
+++ src/index.js
@@ -1,115 +1,145 @@
-//imports 
+//~~~IMPORTS 
 import * as solanaWeb3 from '@solana/web3.js';
-    //import { TokenListProvider, TokenInfo } from '@solana/spl-token-registry';
 import Wallet from '@project-serum/sol-wallet-adapter';
-import { Connection, SystemProgram, Transaction, clusterApiUrl } from '@solana/web3.js';
-import "./styles.css";
+import { Connection, SystemProgram, Transaction, clusterApiUrl, LAMPORTS_PER_SOL, Cluster } from '@solana/web3.js';
 
-//import {GrabStakeAccts} from "./solstake"; HOW DO I?! it's getting hung up on conn, Solanaweb3, and stakekey
-const GrabStakeAccts = async function (pubwallet) {     
-  const stakelist = await conn.getParsedProgramAccounts(stakekey, 
-      {filters: 
-        [ //some guy on discord thinks you can just skip datasize. previously i had {dataSize: 200}
-        {memcmp: {offset: 44, bytes: pubwallet}} //i'd gotten this to work with offset 12 also
-        ]
-      }
-  );
-  //stakelist.forEach((account, index, acctlist) => StakeAdder(account, pubwallet));
-  stakelist.forEach((account, index, acctlist) => StakeAdder(account));
+
+
+//~~~VARIABLES
+let netselect = 'devnet'; //mainnet-beta OR devnet
+let status = 'disconnected';//use this to toggle 
+let solletwallet = new Wallet('https://www.sollet.io');
+let connectedWalletID = ''; //public wallet ID of connected wallet
+let connectedWalletType = ''; //phantom or sollet
+
+
+//~~~FORMULAS
+const SOLsearch = async function (item,index,arry) { 
+  let conn = new solanaWeb3.Connection(clusterApiUrl(netselect)); //mainnet-beta //devnet
+  const pubkey = new solanaWeb3.PublicKey(item);
+  const balance = await conn.getBalance(pubkey) / LAMPORTS_PER_SOL; 
+  console.log('balance captured from: ' + clusterApiUrl(netselect));
+  console.log('Balance : '+ balance + ' SOL');
+  return balance;
 }
-var StakeAdder = function(element) {
-  console.log('mainnet staked amount: '+element.account.lamports /1000000000 + ' SOL' );
+
+//run on any connection (after the .on)
+const connectWallet = async (wallettype) => {
+  connectedWalletType = wallettype;
+  status='connected';
+  console.log("Connected to: "+connectedWalletID); //prints wallet ID
+  document.getElementById("connectedwalletfield").innerHTML = "";    
+  toggleConnectButton(status);
+  let solbalance = await SOLsearch(connectedWalletID);  
+  //document.getElementById("connectedwalletfield").innerHTML += "$SOL Balance: " + solbalance;
+  document.getElementById("sendbox").style.display = "block";
+  document.getElementById("fromWallet").value = 'FoZ1TbVp5aRv1xcSL4bf79zQrC87iAkg9VxVfj9pRhQc'; 
+  document.getElementById("current-balance").value = "$SOL Balance: " + solbalance;
+ 
 }
 
 
-//define variables 
-const SOLSEARCH = async function (item,index,arry) { 
-    const pubkey = new solanaWeb3.PublicKey(item);
-    const balance = await conn.getBalance(pubkey) / 1000000000; 
-    console.log('Balance : '+ balance + ' SOL');
-    return balance;
+//discconect function
+const disconnectWallet = async () => {
+  console.log('Disconnected');
+  status = 'disconnected';
+  connectedWalletType = '';
+  toggleConnectButton(status);
+  document.getElementById("connectedwalletfield").innerHTML = "Disconnected from "+connectedWalletID+"!";
+  document.getElementById("sendbox").style.display = "none";      
+};
+
+const dummyWalletGen = () => {
+  document.getElementById("toWallet").value = '77z6mutuJsUpTa8nsoiUGVv4wan9u1pNbL527zbWJNLF'; 
 }
-let netselect = 'devnet';
-let walletCapture = '';
-let connection = new Connection(clusterApiUrl(netselect)); //mainnet-beta //devnet
-let providerUrl = 'https://www.sollet.io';
-let wallet = new Wallet(providerUrl);
+document.getElementById("toWallet").onclick =  () => {
+  dummyWalletGen();
+}
 
-//establish solanaconnection
-const conn = new solanaWeb3.Connection('https://api.mainnet-beta.solana.com');
-const TokenKey = new solanaWeb3.PublicKey('TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA');
-const stakekey = new solanaWeb3.PublicKey('Stake11111111111111111111111111111111111111'); 
+//~~~BUTTONS
+let toggleConnect= false; //toggle value for connect button
 
-//buttons
-document.getElementById("TestButton1").onclick = async () => {
-    connect2sollet();
- } 
-document.getElementById("TestButton2").onclick = async () => {
-    window.solana.disconnect();
+// Connect Wallet Button opens up the list of options (or closes options)
+document.getElementById("connectbutton").onclick =  () => {
+  if (!toggleConnect){
+    document.getElementById("connectPop").style.display = "block";
+    toggleConnect= true;
+  }
+  else {
+    closeConnect();
+  }
 } 
-document.getElementById("TestButton3").onclick = async () => {
-    window.solana.connect();
-} 
-document.getElementById("TestButton4").onclick = async () => {
-    
-    let transaction = new Transaction().add(
-      SystemProgram.transfer({
-        fromPubkey: wallet.publicKey,
-        toPubkey: new solanaWeb3.PublicKey('5tPMhSJAEcdybJ1BQ8vnGpxUwEvKWdWPyo8tCkAAh93T'),
-        lamports: 1000000000,
-      })
-    );
-    let { blockhash } = await connection.getRecentBlockhash();
-    transaction.recentBlockhash = blockhash;
-    transaction.feePayer = wallet.publicKey;
-    let signed = await wallet.signTransaction(transaction);
-    let txid = await connection.sendRawTransaction(signed.serialize());
-    await connection.confirmTransaction(txid);
-    
-} 
-document.getElementById("TestButton5").onclick = async () => {
-    
-    let transaction = new Transaction().add(
-      SystemProgram.transfer({
-        fromPubkey: window.solana.publicKey,
-        toPubkey: new solanaWeb3.PublicKey('FoZ6TbVp5aRv2xcSL4bf79zQrC87iAkg9VWVfj9pRhQc'),
-        lamports: 100,
-      })
-    );
-    let { blockhash } = await connection.getRecentBlockhash();
-    transaction.recentBlockhash = blockhash;
-    transaction.feePayer = window.solana.publicKey;
-    let signed = await window.solana.signTransaction(transaction);
-    let txid = await connection.sendRawTransaction(signed.serialize());
-    await connection.confirmTransaction(txid);
-    
-} 
-document.getElementById("netdropdown").onclick = async () => {
-    netselect = document.getElementById("netdropdown").options[document.getElementById("netdropdown").selectedIndex].value;
-    console.log('active network: ' + netselect);
-} 
-document.getElementById("TestButton6").onclick = async () => {
-    console.log(contestents[Math.floor(Math.random()*contestents.length)]);
 
+//this will switch connect to DC (and vis versa) when connected/disconnected); must define status and call it into this function
+const toggleConnectButton = (currentstatus) => {
+  if (currentstatus === 'connected'){
+    document.getElementById("connectbutton").style.display = "none";
+    document.getElementById("disconnectbutton").style.display = "block"; 
+  }
+  else {
+    document.getElementById("connectbutton").style.display = "block";
+    document.getElementById("disconnectbutton").style.display = "none"; 
+  }
+}
+
+//phantom button 
+document.getElementById("phantombutton").onclick = async () => {
+  closeConnect(); //close the popup
+  //disconnectWallet(); // first DC anything that's open
+  await window.solana.connect(); //connect to phantom
+}
+//sollet button
+document.getElementById("solletbutton").onclick = async () => {
+  closeConnect(); //close the popup
+  //disconnectWallet(); // first DC anything that's open
+  await solletwallet.connect(); //connect to sollet
+}
+
+//disconnect button
+document.getElementById("disconnectbutton").onclick =  async () => {
+  //disconnect sollet 
+  await solletwallet.disconnect();
+  //disconnect phantom
+  await window.solana.disconnect();
+}
+
+//hide the connect wallet pop-up
+const closeConnect = () =>{
+  document.getElementById("connectPop").style.display = "none";
+  toggleConnect= false; 
+}
+
+// Network Select Button
+document.getElementById("net-toggle").onclick =  () => {
+  closeConnect();
+  //disconnect sollet 
+  solletwallet.disconnect();
+  //disconnect phantom
+  window.solana.disconnect();
+  if (netselect === 'devnet'){
+    document.getElementById("net-toggle").innerHTML = "Main Net"
+    netselect = 'mainnet-beta';
+  }
+  else {
+    document.getElementById("net-toggle").innerHTML = "Dev Net"
+    netselect = 'devnet';
+  }
 } 
+
+
+//~~~WALLET ON.CONNECTS
 
 //sollet connection
-
-wallet.on('connect', publicKey => 
-        {console.log('Connected to ' + publicKey.toBase58());
-        walletCapture = publicKey.toBase58();
-        console.log("Wallet Address: "+walletCapture); //prints wallet ID
-        SOLSEARCH(walletCapture);//finds SOL balance & prints console log
-    
-        }
-
-    );
-wallet.on('disconnect', () => console.log('Disconnected'));
-
-const connect2sollet = async function () {
-    await wallet.connect();
-}
-
+solletwallet.on('connect',  async (publicKey)  =>  {
+  connectedWalletID = publicKey.toBase58();
+  connectWallet('sollet');
+  
+  }
+);
+solletwallet.on('disconnect', () => {
+  disconnectWallet();
+  }
+);
 
 //phantom - check that phantom is installed
 const isPhantomInstalled = window.solana && window.solana.isPhantom; 
@@ -124,70 +154,102 @@ const getProvider = () => {
     window.open("https://phantom.app/", "_blank");
 };
 
-//when connect to solana
-window.solana.on("connect", async () => {
-    console.log("Connected!");
-    walletCapture = window.solana.publicKey.toString(); //defines the public walletID
-    console.log("Wallet Address: "+walletCapture); //prints wallet ID
-    let solbalance = await SOLSEARCH(walletCapture);//finds SOL balance & prints console log
-    document.getElementById('connectionstatus').innerText = 'Connected to ' + walletCapture +'; Balance: '+solbalance;
-    GrabStakeAccts(walletCapture);
+
+//phantom connection 
+// (need to chat w/ austin or dev team getting error about .on sometimes)
+/*window.solana.on("connect", async () => {
+  connectedWalletID = window.solana.publicKey.toString(); //defines the public walletID
+  connectWallet('phantom');
 
 });
 
-//disconnects phantom
-window.solana.on('disconnect', () => {
-    console.log("disconnected!");
-    document.getElementById('connectionstatus').innerText = 'Wallet Disconnected!';
-
+window.solana.on('disconnect', async () => {
+  await disconnectWallet();
 });
+*/
 
-//Phantom Transaction
-const createTransferTransaction = async () => {
-    /*if (!provider.publicKey) {
-      return;
-    }*/
-    let transaction = new Transaction().add(
-      SystemProgram.transfer({
-        fromPubkey: walletCapture,
-        toPubkey: '5tPMhSJAEcdybJ1BQ8vnGpxUwEvKWdWPyo8tCkAAh93T',
-        lamports: 100
-      })
-    );
-    let { blockhash } = await this.connection.getRecentBlockhash();
-    transaction.recentBlockhash = blockhash;
-    transaction.feePayer = walletCapture;
-    let signed = await senderWallet.signTransaction(transaction);
-    let txid = await this.connection.sendRawTransaction(signed.serialize());
-    await this.connection.confirmTransaction(txid);
+setTimeout(function () {
+  window.solana.on("connect", () => 
+    {
+    connectedWalletID = window.solana.publicKey.toString(); //defines the public walletID
+    connectWallet('phantom');
+    }); 
+  window.solana.on('disconnect', async () => {
+    await disconnectWallet();
+  });
+}, 3000);
+
+
+
+
+//~~~TRANSACTIONS
+
+//sollet
+const solletsend = async () =>{
+  let conn = new solanaWeb3.Connection(clusterApiUrl(netselect)); //mainnet-beta //devnet
+  let toWallet = document.getElementById("toWallet").value;
+  let sendAmt = document.getElementById("sendamt").value * LAMPORTS_PER_SOL; //number of lamports to send
+  let sendType = document.getElementById("token").value; //token to send (default solana)
+
+  let transaction = new Transaction().add(
+    SystemProgram.transfer({
+      fromPubkey: solletwallet.publicKey,
+      toPubkey: new solanaWeb3.PublicKey(toWallet),
+      lamports: sendAmt,
+    })
+  );
+  let { blockhash } = await conn.getRecentBlockhash();
+  transaction.recentBlockhash = blockhash;
+  transaction.feePayer = solletwallet.publicKey;
+  let signed = await solletwallet.signTransaction(transaction);
+  let txid = await conn.sendRawTransaction(signed.serialize());
+  let txConfirm = await conn.confirmTransaction(txid);
+  console.log(txConfirm);
 }
-const makeTransaction = async () => {
-    
-    let transaction =  new Transaction().add(
-        SystemProgram.transfer({
-          fromPubkey: walletCapture,
-          toPubkey: '5tPMhSJAEcdybJ1BQ8vnGpxUwEvKWdWPyo8tCkAAh93T',
-          lamports: 100
-        }))
-     console.log(transaction);
-     let { blockhash } = await this.connection.getRecentBlockhash();
-     transaction.recentBlockhash = blockhash;
-     transaction.feePayer = walletCapture;
-     let signed = await senderWallet.signTransaction(transaction);
-     let txid = await this.connection.sendRawTransaction(signed.serialize());
-     await this.connection.confirmTransaction(txid);
+
+document.getElementById("submit-button").onclick = async () =>{
+
+  //if (document.getElementById("sendamt").value * LAMPORTS_PER_SOL > balance){}
+  if (connectedWalletType==="sollet"){
+    solletsend();
+  }
+  else if (connectedWalletType==="phantom"){
+    phantomsend();
+  }
+  else {console.error();}
+
 }
 
+//phantom send
+const phantomsend = async () => {
+  let conn = new solanaWeb3.Connection(clusterApiUrl(netselect)); //mainnet-beta //devnet
+  let toWallet = document.getElementById("toWallet").value;
+  let sendAmt = document.getElementById("sendamt").value * LAMPORTS_PER_SOL; //number of lamports to send
+  let sendType = document.getElementById("token").value; //token to send (default solana)
 
-/* LINKS:
-Phantom: https://docs.phantom.app/integrating/establishing-a-connection
-LEft off here: 
-https://github.com/phantom-labs/sandbox/blob/main/src/App.tsx
-https://docs.phantom.app/integrating/sending-a-transaction
-https://codesandbox.io/s/github/phantom-labs/sandbox?file=/src/App.tsx
+  let transaction = new Transaction().add(
+    SystemProgram.transfer({
+      fromPubkey: window.solana.publicKey,
+      toPubkey: new solanaWeb3.PublicKey(toWallet),
+      lamports: sendAmt,
+    })
+  );
+  let { blockhash } = await conn.getRecentBlockhash();
+  transaction.recentBlockhash = blockhash;
+  transaction.feePayer = window.solana.publicKey;
+  let signed = await window.solana.signTransaction(transaction);
+  let txid = await conn.sendRawTransaction(signed.serialize());
+  let txConfirm = await conn.confirmTransaction(txid);
+  console.log(txConfirm);
+}
+
+/* 
 
 
 
+phantom!
+need to respond to the transaction promise. e.g. (tx confirmed!; update balance; )
+need to add responsive "not enough balance and do not allow click if balance too low"
+possibly some logic to prevent clicking based on those values. need to make a balance global variable or pass results somewhere
 
 */
-const contestents = ['blumber','muhammed','mrFro','firmin','adesoye','chris'];
